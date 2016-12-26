74b8314628423eda3adea8e777f0c9c6a506ad90
Marc Brakken <marc.brakken@gmail.com>
2016-12-26 11:40:36 -0600

    switch to library hashing method and chai expectations

--- test/validate.js
+++ test/validate.js
@@ -1,19 +1,13 @@
-process.env.NODE_ENV = "test";
-
-var assert = require("assert");
-var crypto = require("crypto");
+var expect   = require('chai').expect
 var validate = require("../lib/validate.js");
+var log      = require("../jlog.js");
+var config   = require("../config.js");
+var utils    = require("../lib/utils.js");
 
 var GOOD_KEY = "testing_key";
 var BAD_KEY  = "wrong_key";
 var INODE    = { access_key: GOOD_KEY };
 var GET      = "GET";
-
-function createToken(data){
-  var sha = crypto.createHash("sha1");
-  sha.update(data);
-  return sha.digest("hex");
-}
 
 function setExpire(minutes){
   var d = new Date();
@@ -23,17 +17,29 @@ function setExpire(minutes){
 
 describe("validation.js", function(){
 
+  before(function(){
+    // suppress debug log output for tests
+    log.level = 4;
+  });
+
+  after(function(){
+    // restore default log level
+    log.level = config.LOG_LEVEL
+  });
+
   describe("#has_key(inode, params)", function() {
     it("should validate an access_key", function() {
       var params = { access_key: GOOD_KEY };
+      var result = validate.has_key(INODE, params);
 
-      assert.ok(validate.has_key(INODE, params));
+      expect(result).to.be.true;
     });
 
     it("should reject an incorrect access_key", function() {
       var params = { access_key: BAD_KEY };
+      var result = validate.has_key(INODE, params);
 
-      assert.equal(validate.has_key(INODE, params), false);
+      expect(result).to.be.false;
     });
 
   });
@@ -42,64 +48,73 @@ describe("validation.js", function(){
 
     it("should validate an access_key", function() {
       var params = { access_key: GOOD_KEY };
+      var result = validate.is_authorized(INODE, GET, params);
 
-      assert.ok(validate.is_authorized(INODE, GET, params));
+      expect(result).to.be.true;
     });
 
     it("should reject an incorrect access key", function(){
       var params = { access_key: BAD_KEY };
+      var result = validate.is_authorized(INODE, GET, params);
 
-      assert.equal(validate.is_authorized(INODE, GET, params), false);
+      expect(result).to.be.false;
     });
 
     it("should validate an access token", function(){
-      var params = { access_token: createToken(GOOD_KEY + GET) };
+      var params = { access_token: utils.sha1_to_hex(GOOD_KEY + GET) };
+      var result = validate.is_authorized(INODE, GET, params);
 
-      assert.ok(validate.is_authorized(INODE, GET, params));
+      expect(result).to.be.true;
     });
 
     it("should reject an access token for wrong method", function(){
-      var params = { access_token: createToken(GOOD_KEY + "POST") };
+      var params = { access_token: utils.sha1_to_hex(GOOD_KEY + "POST") };
+      var result = validate.is_authorized(INODE, GET, params);
 
-      assert.equal(validate.is_authorized(INODE, GET, params), false);
+      expect(result).to.be.false;
     });
 
     it("should reject wrong access token", function(){
-      var params = { access_token: createToken(BAD_KEY + GET) };
+      var params = { access_token: utils.sha1_to_hex(BAD_KEY + GET) };
+      var result = validate.is_authorized(INODE, GET, params);
 
-      assert.equal(validate.is_authorized(INODE, GET, params), false);
+      expect(result).to.be.false;
     });
 
     it("should validate a future time token", function() {
       var expires = setExpire(30);
       var params  = {
-        access_token : createToken(GOOD_KEY + GET + expires),
+        access_token : utils.sha1_to_hex(GOOD_KEY + GET + expires),
         expires      : expires
       };
+      var result = validate.is_authorized(INODE, GET, params)
 
-      assert.ok(validate.is_authorized(INODE, GET, params));
+      expect(result).to.be.true;
     });
 
     it("should reject an expired time token", function() {
       var expires = setExpire(-1);
       var params = {
-        access_token : createToken(GOOD_KEY + GET + expires),
+        access_token : utils.sha1_to_hex(GOOD_KEY + GET + expires),
         expires      : expires
       };
+      var result = validate.is_authorized(INODE, GET, params);
 
-      assert.equal(validate.is_authorized(INODE, GET, params), false);
+      expect(result).to.be.false;
     });
 
     it("should validate HEAD requests", function(){
-      var params = { access_token: createToken(BAD_KEY + GET) };
+      var params = { access_token: utils.sha1_to_hex(BAD_KEY + GET) };
+      var result = validate.is_authorized(INODE, "HEAD", params);
 
-      assert.ok(validate.is_authorized(INODE, "HEAD", params));
+      expect(result).to.be.true;
     });
 
     it("should validate OPTIONS requests", function(){
-      var params = { access_token: createToken(BAD_KEY + GET) };
+      var params = { access_token: utils.sha1_to_hex(BAD_KEY + GET) };
+      var result = validate.is_authorized(INODE, "OPTIONS", params);
 
-      assert.ok(validate.is_authorized(INODE, "OPTIONS", params));
+      expect(result).to.be.true;
     });
 
   });
